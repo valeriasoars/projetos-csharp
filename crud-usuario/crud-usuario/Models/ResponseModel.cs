6b719da3cd06e33e66fee65a8190b70ecd4ca0be
valeriasoars <valeriasoaressantos2@gmail.com>
2026-04-22 09:41:35 -0300

    crud usuario: criando response model

---
+++ crud-usuario/crud-usuario/Models/ResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace crud_usuario.Models
+{
+    public class ResponseModel<T>
+    {
+        public T? Dados { get; set; }
+        public string Mensagem { get; set; }
+        public bool Status { get; set; }
+    }
+}
