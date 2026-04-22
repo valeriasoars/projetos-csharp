ce1b835680ccde23d419e350659c9c6307e3f13a
valeriasoars <valeriasoaressantos2@gmail.com>
2026-04-22 09:37:34 -0300

    crud usuario: criando tabela de usuarios

--- crud-usuario/crud-usuario/Data/AppDbContext.cs
+++ crud-usuario/crud-usuario/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using crud_usuario.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace crud_usuario.Data
 {
@@ -7,5 +8,7 @@ namespace crud_usuario.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }
     }
 }
