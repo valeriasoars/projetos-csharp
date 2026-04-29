5e77ffb4e886539f0fd932da8541f70c5e9433d3
valeriasoars <valeriasoaressantos2@gmail.com>
2026-04-29 10:16:31 -0300

    criação dto

---
+++ crud-usuario/crud-usuario/Dto/Usuario/UsuarioCriacaoDto.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace crud_usuario.Dto.Usuario
+{
+    public class UsuarioCriacaoDto
+    {
+
+        [Required(ErrorMessage = "O campo Usuário é obrigatório.")]
+        public string Usuario { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo Sobrenome é obrigatório.")]
+        public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "O campo Email é obrigatório.")]
+        public string Email { get; set; }
+
+
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataAlteracao { get; set; }
+       
+
+        [Required(ErrorMessage = "O campo Senha é obrigatório.")]
+        public string senha { get; set; }
+
+        [Required(ErrorMessage = "O campo Confirma Senha é obrigatório.")]
+        [Compare("senha", ErrorMessage = "As senhas não coincidem.")]
+        public string confirmaSenha{ get; set; }
+    }
+}
---
+++ crud-usuario/crud-usuario/Dto/Usuario/UsuarioEdicaoDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace crud_usuario.Dto.Usuario
+{
+    public class UsuarioEdicaoDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Usuário é obrigatório.")]
+        public string Usuario { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo Sobrenome é obrigatório.")]
+        public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "O campo Email é obrigatório.")]
+        public string Email { get; set; }
+
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataAlteracao { get; set; }
+
+    }
+}
